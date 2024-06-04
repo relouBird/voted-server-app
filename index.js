c8fcafc09ca3036cb7851e5fe885d2ad719c384c
RelouBird <ulrichkwamou@gmail.com>
2024-06-04 21:15:12 +0100

    all is up to date.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const io = require("socket.io")(8000, { cors: { origin: "*" } });
+// const io = require("socket.io")(8000, { cors: { origin: "*" } });
+const express = require("express");
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" }, connectionStateRecovery: {} });
 
 let totalVoting = 0;
 
@@ -7,7 +14,15 @@ let votingPolls = {
   "I'm good person.": 0,
 };
 
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
 io.on("connection", socket => {
+  console.log('a user connected');
+
   socket.emit("update", { totalVoting, votingPolls });
   socket.on("send-vote", (vote) => {
     totalVoting++;
@@ -16,4 +31,8 @@ io.on("connection", socket => {
     socket.broadcast.emit("receive-vote", { totalVoting, votingPolls });
     io.emit("update", { totalVoting, votingPolls });
   });
+});
+
+server.listen(8000, () => {
+  console.log('server running at http://localhost:8000');
 });
