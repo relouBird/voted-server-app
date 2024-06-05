a09b4884774ba7875149110674cb96711cf7650f
RelouBird <ulrichkwamou@gmail.com>
2024-06-05 19:03:03 +0100

    all is complete about vote

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-// const io = require("socket.io")(8000, { cors: { origin: "*" } });
-const express = require("express");
-const { createServer } = require('node:http');
-const { Server } = require('socket.io');
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+} from "firebase/firestore";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, { cors: { origin: "*" }, connectionStateRecovery: {} });
+const io = new Server(server, {
+  cors: { origin: "*" },
+  connectionStateRecovery: {},
+});
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ5Uv4lnOoYYNHtKUWzlwom5B96dhNpS4",
+  authDomain: "voted-web-app-9e54c.firebaseapp.com",
+  projectId: "voted-web-app-9e54c",
+  storageBucket: "voted-web-app-9e54c.appspot.com",
+  messagingSenderId: "213625233671",
+  appId: "1:213625233671:web:bdee62afcb0bc157f725a2",
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(appFirebase);
+
+let data;
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  // console.log(`${doc.id} => ${doc.data()}`);
+  data = doc.data();
+});
 
 let totalVoting = 0;
 
@@ -14,25 +47,65 @@ let votingPolls = {
   "I'm good person.": 0,
 };
 
+if (data) {
+  totalVoting = data.totalVoting;
+  votingPolls = data.votingPolls;
+}
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
 
-io.on("connection", socket => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
   socket.emit("update", { totalVoting, votingPolls });
-  socket.on("send-vote", (vote) => {
+  socket.on("send-vote", async (vote) => {
     totalVoting++;
     votingPolls[vote]++;
+    try {
+      const docRef = await setDoc(doc(db, "users", "voting-session"), {
+        totalVoting: totalVoting,
+        votingPolls: votingPolls,
+      });
+      console.log("Document written with ID: voting-session");
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        totalVoting = doc.data().totalVoting;
+        votingPolls = doc.data().votingPolls;
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
     console.log(vote);
     socket.broadcast.emit("receive-vote", { totalVoting, votingPolls });
     io.emit("update", { totalVoting, votingPolls });
   });
+
+  // ceci est fichier test
+  // permet de creer un user dans la bd
+  // socket.emit("update-message", data);
+  // socket.on("message", async (message) => {
+  //   try {
+  //     const docRef = await setDoc(doc(db, "users", "voting-session"), {
+  //       userId: message,
+  //     });
+  //     console.log("Document written with ID: voting-session");
+  //     const querySnapshot = await getDocs(collection(db, "users"));
+  //     querySnapshot.forEach((doc) => {
+  //       data = doc.data();
+  //     });
+  //   } catch (e) {
+  //     console.error("Error adding document: ", e);
+  //   }
+  //   io.emit("update-message", data);
+  // });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(8000, () => {
-  console.log('server running at http://localhost:8000');
+  console.log("server running at http://localhost:8000");
 });
